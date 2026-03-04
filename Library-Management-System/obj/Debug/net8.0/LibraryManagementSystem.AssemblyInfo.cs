e44682262bb284c651152dbec0880b7e6de30c14
HoangGiang310 <kenzakishyina@gmail.com>
2026-03-04 15:23:10 +0700

    Search Toolbar

--- Library-Management-System/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ Library-Management-System/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b19f45d8d7a18a7dbbb199fd47a213d6d3ed315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e1aeef8f036259e46895cdf418dd29170fb70b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
