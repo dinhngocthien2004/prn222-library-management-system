c912d4985f62597eb2600bb69b7d23a91136d2ca
HoangGiang310 <kenzakishyina@gmail.com>
2026-03-15 21:59:54 +0700

    Update Member And Login UI

--- Library-Management-System/Controllers/AccountController.cs
+++ Library-Management-System/Controllers/AccountController.cs
@@ -9,56 +9,61 @@ namespace Library_Management_System.Controllers
     {
         private readonly IUserService _accounts;
 
-    public AccountController(IUserService accounts)
+        public AccountController(IUserService accounts)
         {
             _accounts = accounts;
         }
 
-        // Hiển thị trang Login  
+        // Hiển thị trang Login
         [HttpGet]
         public IActionResult Login()
         {
+            // Nếu đã đăng nhập rồi thì quay về trang chủ
+            if (HttpContext.Session.GetString("Username") != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
 
-        // Xử lý Login  
+        // Xử lý Login
         [HttpPost]
         public IActionResult Login(string email, string password)
         {
             var user = _accounts.Login(email, password);
 
-            // Sai tài khoản  
+            // Sai tài khoản
             if (user == null)
             {
                 ViewBag.Error = "Invalid email or password!";
                 return View();
             }
 
-            // Tài khoản bị khóa  
+            // Tài khoản bị khóa
             if (user.IsActive == false)
             {
                 ViewBag.Error = "Your account has been locked!";
                 return View();
             }
 
-            // Xóa session cũ (nếu có)  
+            // Xóa session cũ
             HttpContext.Session.Clear();
 
-            // Lưu thông tin vào session  
+            // Lưu thông tin vào session
             HttpContext.Session.SetString("UserId", user.UserId.ToString());
             HttpContext.Session.SetString("Username", user.FullName);
             HttpContext.Session.SetInt32("RoleID", user.RoleId);
 
-            // Sau khi login → về trang chủ  
+            // Login xong → về Trang Chủ
             return RedirectToAction("Index", "Home");
         }
 
-        // Logout  
+        // Logout
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Login", "Account");
         }
-    }  
-
+    }
 }
