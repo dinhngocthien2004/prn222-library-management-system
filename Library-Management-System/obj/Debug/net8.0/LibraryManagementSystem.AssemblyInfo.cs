94b43b3ef49473d06ae89de25803f1d9c32375a3
Henry9104 <tminhquan912004@gmail.com>
2026-03-18 20:17:59 +0700

    edit reader

--- Library-Management-System/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ Library-Management-System/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3057f0bd539d8470e55b8fec9f08ca4aa9cb3a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559a4ac60a22a317f4a160823664f20180219d88")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
